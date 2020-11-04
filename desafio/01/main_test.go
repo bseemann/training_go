cc6a6fed0bd342c4f7ac9dc9550f15c064f65a48
ENDURANCE <suporte@brfl-2687-l.corp.endurance.com>
2020-11-04 11:48:09 -0300

    desafio 01

--- desafio/01/main_test.go
+++ desafio/01/main_test.go
@@ -4,21 +4,21 @@ import "testing"
 
 func TestBigger(t *testing.T) {
 	g := bigger(12)
-	e := 24
+	e := 24.0
 	// Case even number greater than 10
 	if g != e {
 		t.Errorf("Exp: %v, Got: %v", e, g)
 	}
 
 	g = bigger(13)
-	e = 26
+	e = 26.0
 	// Case odd number greater than 10
 	if g != e {
 		t.Errorf("Exp: %v, Got: %v", e, g)
 	}
 
 	g = bigger(8)
-	e = 4
+	e = 4.0
 	// Case even number lesser than 10
 	if g != e {
 		t.Errorf("Exp: %v, Got: %v", e, g)
@@ -32,7 +32,7 @@ func TestBigger(t *testing.T) {
 	}
 
 	g = bigger(10)
-	e = 10
+	e = 10.0
 	// Case number is 10
 	if g != e {
 		t.Errorf("Exp: %v, Got: %v", e, g)
